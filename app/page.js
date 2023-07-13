8d78cba6e4ee79d0709e5cea418a65d69199df4e
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-14 05:23:42 +0600

    Google Authentication Added

--- app/page.js
+++ app/page.js
@@ -5,14 +5,18 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
   const { data: session } = useSession()
   if (!session){
-    return 'not logged in';
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
+     <div className="text-center w-full">
+      <button onClick={()=> signIn('google')} className="bg-white p-4 rounded-lg text-blue-900 font-semibold">LogIn with Google</button>
+     </div>
+    </div>
+    )
   }
 
   return(
     <div className='bg-blue-900 w-screen h-screen flex items-center'>
-     <div className="text-center w-full">
-      <button className="bg-white p-4 rounded-lg text-blue-900 font-semibold">LogIn with Google</button>
-     </div>
+     Logged in {session?.user?.name}
     </div>
   )
 }
