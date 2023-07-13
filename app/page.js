64add272bdee4884090db37984ff82756c715ec7
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-14 05:17:46 +0600

    Authentication Added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
-import Image from 'next/image'
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+
 
 export default function Home() {
+  const { data: session } = useSession()
+  if (!session){
+    return 'not logged in';
+  }
+
   return(
-    <div className='flex flex-col items-center'>
-      <h3>Future Ecommerce Admin</h3>
-      <h3>Another test on env</h3>
+    <div className='bg-blue-900 w-screen h-screen flex items-center'>
+     <div className="text-center w-full">
+      <button className="bg-white p-4 rounded-lg text-blue-900 font-semibold">LogIn with Google</button>
+     </div>
     </div>
   )
 }
+
+
