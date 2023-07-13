20fcfd0ffaadbeb0f8827c6bb59bb3c95eca35b5
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-14 00:45:06 +0600

    testing env

--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return(
-    <div>
+    <div className='flex flex-col items-center'>
       <h3>Future Ecommerce Admin</h3>
+      <h3>Another test on env</h3>
     </div>
   )
 }
