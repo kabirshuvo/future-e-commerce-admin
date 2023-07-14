d34fee103f396482afa3ada54586584accbc3238
kabir shuvo <kabirshuvo19@gmail.com>
2023-07-14 17:43:25 +0600

    orders and settings route created

--- app/page.js
+++ app/page.js
@@ -18,15 +18,15 @@ export default function Home() {
 
   return(
    <div >
-    <header className='bg-blue-900 w-screen h-screen flex flex-col text-white p-4'>
+    <header className='bg-blue-900 w-screen h-screen flex text-white p-4'>
 <Navbar/>
 
-<div className='bg-blue-900 w-screen h-screen flex mt-4'>
+<div className='bg-blue-900 w-screen h-screen flex grow-1 bg-white text-blue-950 p-1 rounded-lg'>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
 </svg>
 
-    <span className="ps-1"> Logged in {session?.user?.name}</span>
+    <span className="ps-1"><span className="p-1 font-bold">{session?.user?.name}</span> Logged in </span>
     </div>
     </header>
    
